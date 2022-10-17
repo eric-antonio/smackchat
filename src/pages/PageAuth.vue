09f575a5838536acef26581f2878f1c2d6a4e09f
Éric Ivan João António <ericantoniodev@gmail.com>
2022-10-17 14:09:45 +0200

    Create PageAuth.vue

---
+++ src/pages/PageAuth.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex ">
+
+    <h5>Page Auth</h5>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+
+})
+</script>
+
+<style>
+</style>
