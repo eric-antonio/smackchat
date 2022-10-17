3b5416c4f35996dc5a1352f93f3a6ade4a9ae3c4
Éric Ivan João António <ericantoniodev@gmail.com>
2022-10-17 14:08:16 +0200

    Chat Page Created

---
+++ src/pages/PageUsers.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex ">
+
+    <h5>User Page</h5>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  
+})
+</script>
+
+<style>
+</style>
