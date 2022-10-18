8cec5acbf4ed3ac04f9126b15072c8592dbd93e5
Éric Ivan João António <ericantoniodev@gmail.com>
2022-10-18 18:57:42 +0200

    Update PageChat.vue
    Não manda mensagems!

--- src/pages/PageChat.vue
+++ src/pages/PageChat.vue
@@ -5,36 +5,37 @@
       <q-chat-message
       v-for="message in messages"
         :key="message.text"
-        
         :name="[message.from]"
         :text="[message.text]"
         :sent="message.from =='me' ? true: false"
       />
     </div>
     <q-footer elevated>
+  	  <q-toolbar>
+  	  	<q-form
+  	  		@submit="sendMessage"
+  	  		class="full-width">
+	  	    <q-input
+	  	    	v-model="newMessage"
+	  	    	bg-color="white"
+	  	    	outlined
+	  	    	rounded
+	  	    	label="Message"
+	  	    	dense>
 
-        <q-toolbar>
-
-         <q-input
-            class="full-width"
-            id="label"
-            outlined
-            bg-color="white"
-            rounded
-            v-model="newMessage"
-            label="Message"
-            dense>
-
-            <template v-slot:after>
-              <q-btn round dense flat icon="send"  color="white"/>
-            </template>
-
-          </q-input>
-
-        </q-toolbar>
-
-      </q-footer>
-
+	  	      <template v-slot:after>
+	  	        <q-btn
+	  	        	round
+	  	        	dense
+	  	        	flat
+	  	        	type="submit"
+	  	        	color="white"
+	  	        	icon="send" />
+	  	      </template>
+	  	    </q-input>
+  	  	</q-form>
+  	  </q-toolbar>
+  	</q-footer>
   </q-page>
 </template>
 
@@ -59,13 +60,20 @@ export default defineComponent({
         }
       ]
     }
+  },
+  methods:{
+    sendMessage(){
+      this.messages.push({
+        text: this.newMessage,
+        from:'me'
+
+      })
+    }
   }
 
 })
 </script>
 
 <style>
-  #label{
-    margin-bottom: 20px;
-  }
+
 </style>
