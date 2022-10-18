b3b97c802193393e55ae33b02cbbb2418d80cf65
Éric Ivan João António <ericantoniodev@gmail.com>
2022-10-18 17:38:43 +0200

    Update PageChat.vue

--- src/pages/PageChat.vue
+++ src/pages/PageChat.vue
@@ -2,6 +2,30 @@
   <q-page class="flex ">
 
     <h5> Chat page</h5>
+    <q-footer elevated>
+
+        <q-toolbar>
+
+         <q-input
+            class="full-width"
+            id="label"
+            outlined
+            bg-color="white"
+            rounded
+            v-model="text"
+            label="Message"
+            dense>
+
+            <template v-slot:after>
+              <q-btn round dense flat icon="send"  color="white"/>
+            </template>
+
+          </q-input>
+
+        </q-toolbar>
+
+      </q-footer>
+
   </q-page>
 </template>
 
@@ -14,4 +38,7 @@ export default defineComponent({
 </script>
 
 <style>
+  #label{
+    margin-bottom: 20px;
+  }
 </style>
