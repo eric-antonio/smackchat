4a942a1947d9ae240689b60413e003b33d804d7b
Éric Ivan João António <ericantoniodev@gmail.com>
2022-10-18 11:30:36 +0200

    SmackChat Page Done✅

--- src/pages/PageUsers.vue
+++ src/pages/PageUsers.vue
@@ -1,16 +1,72 @@
 <template>
-  <q-page class="flex ">
+  <q-page class="flex  q-pa-md" >
 
-    <h5>User Page</h5>
+
+    <q-list
+    class="full-width" separator>
+      <q-item
+        v-for="user in users"
+        :key="user.id"
+        to="/chat"
+        clickable
+        v-ripple>
+        <q-item-section avatar>
+          <q-avatar color="primary" text-color="white">
+            {{ user.name.charAt(0) }}
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ user.name }}</q-item-label>
+
+        </q-item-section>
+
+        <q-item-section side>
+
+          <q-badge :color="user.online? 'green-6' : 'grey-4'">
+            {{user.online? 'Online': 'Ofline'}}
+          </q-badge>
+
+        </q-item-section>
+
+      </q-item>
+
+    </q-list>
+
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+  import { defineComponent } from 'vue'
 
-export default defineComponent({
-  
-})
+  // Script da q-list
+export default {
+  setup () {
+    return {
+      users:[ {
+        id: 1,
+        name: 'Ruddy Jedrzej',
+        online: true,
+      }, {
+        id: 2,
+        name: 'Mallorie Alessandrini',
+        online: false,
+      }, {
+        id: 3,
+        name: 'Elisabetta Wicklen',
+        online: true,
+      }, {
+        id: 4,
+        name: 'Seka Fawdrey',
+        online: false,
+      }, {
+        id: 5,
+        name: 'Fernada Fredericky',
+        online: true,
+      }]
+    }
+  }
+}
 </script>
 
 <style>
